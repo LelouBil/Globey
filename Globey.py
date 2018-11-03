1f4858beffd0432cb8273b4bf9f9e820dafda877
Aurelien chambolle <aure.chambollesolaz@gmail.com>
2018-11-03 11:53:47 +0100

    ca ca marche pas

--- Globey.py
+++ Globey.py
@@ -1,7 +1,6 @@
 import asyncio
 import discord
 from discord.ext import commands
-import sqlite3
 import random
 
 import time
@@ -9,68 +8,10 @@ import time
 Client = discord.Client
 client = commands.Bot(command_prefix="_")
 
-sqlite = sqlite3.connect("/storage/database.db")
-cursor = sqlite.cursor()
-cursor.execute("""
-
-CREATE TABLE IF NOT EXISTS servers
-(
- `server_id`   int NOT NULL,
- `server_name` text NOT NULL ,
-
-PRIMARY KEY (`server_id`)
-);
-""")
-cursor.execute("""
-CREATE TABLE IF NOT EXISTS global_channels (
-    channel_id integer NOT NULL,
-    server_id integer NOT NULL
-, channel_name TEXT NOT NULL,
-PRIMARY KEY (`channel_id`),
-FOREIGN KEY(server_id) REFERENCES servers(server_id)
-);
-""")
-
-
-def getCursor():
-    return sqlite.cursor()
-
-
-def register_server(srv: discord.server.Server):
-    name = srv.name
-    idd = srv.id
-    getCursor().execute("INSERT INTO servers (server_id,server_name) VALUES (?,?)", (idd, name))
-    sqlite.commit()
-
-
-def register_channel(chan: discord.server.Channel):
-    name = chan.name
-    idd = chan.id
-    srvid = chan.server.id
-    getCursor().execute("INSERT INTO global_channels (channel_id,channel_name,server_id) VALUES (:id,:name,:srvid)",
-                        {"id": idd, "name": name, "srvid": srvid})
-    sqlite.commit()
-
-
-def unregister_channel(chan: discord.server.Channel):
-    idd = chan.id
-    getCursor().execute(f"DELETE FROM global_channels WHERE channel_id=:id", {"id": idd})
-    sqlite.commit()
-
-
-def registered(srv: discord.server.Server):
-    c = getCursor()
-    c.execute(f"SELECT EXISTS(SELECT 1 FROM servers WHERE server_id={srv.id})")
-    return c.fetchone() == (1,)
-
 
 @client.event
 async def on_ready():
     print("Bot Is Online!")
-    for srv in client.servers:
-        if not registered(srv):
-            print("registering server : " + srv.name)
-            register_server(srv)
     await client.change_presence(game=discord.Game(name="linking people"))
 
 
@@ -205,111 +146,36 @@ async def spam(ctx):
     await client.say(" ")
     await client.say(" ")
 
-
 @client.command(pass_context=True)
 async def globaldef(ctx):
-    if isglobal(ctx.message.channel):
-        await client.say("This channel is already global !")
-        return
     await client.say("this channel is now set as a global channel")
-    channel = ctx.message.channel  # here I get the channel
-    register_channel(channel)
-
-
+    channel = client.get_channel #here I get the channel
+    print(channel) #put in data base
+    
 @client.command(pass_context=True)
 async def globalstop(ctx):
-    if not isglobal(ctx.message.channel):
-        await client.say("This channel is not global !")
-        return
     await client.say("this channel is not anymore set as a global channel")
-    channel = ctx.message.channel
-    unregister_channel(channel)
-
-
+    #here just erease from database
+    
 @client.command(pass_context=True)
 async def invite(ctx):
     await client.say("https://discordapp.com/api/oauth2/authorize?client_id=456478882577645568&permissions=8&scope=bot")
 
 
-def getGlobalChannels():
-    c = getCursor()
-    c.execute("SELECT * FROM global_channels")
-    rows = c.fetchall()
-    channels = list()
-    for row in rows:
-        cid = row[0]
-        sid = row[1]
-        chanel = client.get_server(str(sid)).get_channel(str(cid))
-        channels.append(chanel)
-    return channels
-
-
-def isglobal(channel: discord.channel.Channel):
-    c = getCursor()
-    c.execute(f"SELECT EXISTS(SELECT 1 FROM global_channels WHERE channel_id={channel.id})")
-    return c.fetchone() == (1,)
-
-
-noRepeat = {"_globaldef", "_globalstop"}
-
-# regexes
-import re
-
-mentionreg = re.compile("<@(\d+)>")
-
-everyhere = re.compile("(@)(everyone|here)")
-
-linkreg = re.compile("(http|ftp|https)://([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?")
-
-
-async def filterMessage(content: str):
-    matchobj = mentionreg.search(content)
-    if matchobj is not None:
-        toreplace = matchobj.group()
-        id = matchobj.group(1)
-        print("mention of : " + id + " ---- " + toreplace)
-        user = await client.get_user_info(str(id))
-        username = str(user)
-        content = content.replace(toreplace, username)
-        # fin mentions
-
-    # links
-
-    match = linkreg.search(content)
-    if match is not None:
-        link = match.group()
-        content = content.replace(link, "_[link are disabled here]_")
-
-    #everyHere
-
-    ma = everyhere.search(content)
-    if ma is not None:
-        tore = ma.group(1)
-        ot = ma.group(2)
-        #print("mention of : " + id + " ---- " + toreplace)
-        content = content.replace(ma.group(), tore + " " + ot)
-        # fin mentions
-    return content
-
-
 @client.event
 async def on_message(message):
-    if isglobal(message.channel):
-        if not noRepeat.__contains__(message.content):
-            filtered = await filterMessage(message.content)
-            if not message.author.bot:
-                channel = getGlobalChannels()
-                for i in channel:
-                    if i.type != discord.channel.ChannelType.private:
-                        try:
-                            await client.send_message(i, f"**[{message.author}@{message.server}]** {filtered}")
-                        except discord.errors.Forbidden:
-                            print(f"forbidden channel : {i.name}@{i.server.name}")
+    if message.channel.name == "global-chat":
+        if not message.author.bot:
+            channel = client.get_all_channels()
+            for i in channel:
+                if i.name == "global-chat" and i.type != discord.channel.ChannelType.private:
+                    try:
+                        await client.send_message(i, f"**[{message.author}@{message.server}]** `{message.content}`")
+                    except discord.errors.Forbidden:
+                        print(f"forbidden channel : {i.name}@{i.server.name}")
 
     if message.content.startswith("cookie"):
-        await client.send_message(message.channel, ":cookie:")
-    if message.content.startswith("thinking"):
-        await client.send_message(message.channel, ":thinking:")
+        await client.add_reaction(message,"\N{COOKIE}")
     if message.content.startswith("hello"):
         embed1 = discord.Embed(title="Hello :wave:", description=f"Hello {message.author.name}, How are you?",
                                color=message.author.color)
@@ -317,24 +183,22 @@ async def on_message(message):
         embed1.set_footer(text="send by god")
         await client.send_message(message.channel, embed=embed1)
     if message.content.find(':EmojiName:'):
-        for x in client.get_all_emojis():
-            if x.id == '#EmojiID#':
-                return await client.add_reaction(message, x)
+        for i in client.get_all_emojis():
+            if i.id == '#EmojiID#':
+                return await client.add_reaction(message.channel, i)
     if message.content.startswith("help"):
         embed2 = discord.Embed(title="**Help menu**",
-                               description=f"**Hello {message.author.name}, here are all comands (with prefix_):** \n"
-                                           f" hi, invite, server, redriot, amiuseful, spam, invite. \n "
-                                           f"**admin command:**\n ban, warn, kick",
+                               description=f"**Hello {message.author.name}, here are all comands (with prefix_):** \n hi, invite, server, redriot, amiuseful, spam, invite. \n **admin command:**\n ban, warn, kick",
                                color=message.author.color)
         embed2.set_thumbnail(url=message.author.avatar_url)
         embed2.set_footer(text="send by god")
         await client.send_message(message.channel, embed=embed2)
     if message.content.startswith("party"):
-        await client.send_message(message.channel, ":tada:")
+        await client.add_reaction(message,"\N{TADA}")
     if message.content.startswith("lol"):
-        await client.send_message(message.channel, ":rofl:")
-    if message.content.startswith("nooo"):
-        await client.send_message(message.channel, ":facepalm:")
+        await client.add_reaction(message,"\N{ROLF}")
+    if message.content.startswith("no"):
+        await client.add_reaction(message,"\N{FACEPALM}")
     await client.process_commands(message)
 
 
